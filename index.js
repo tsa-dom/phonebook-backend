c723b799be7f7cf03e9f7e93fa314be588550bef
Tapio Salonen <tsa.dom@pm.me>
2021-03-29 00:16:46 +0300

    Bug fixed

--- index.js
+++ index.js
@@ -72,10 +72,13 @@ app.post('/api/persons', (req, res, next) => {
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndUpdate(req.params.id)
-    .then(person => {
-      if (person) person.save().then(savedPerson => res.json(savedPerson))
-    })
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => res.json(updatedPerson))
     .catch(error => next(error))
 })
 
